1d95fb53ea142adaea9a92f5e8d90999a8e709e5
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-22 20:05:23 +0300

    declared a basic functionality for an item

--- ENGINE/Item/Item.h
+++ ENGINE/Item/Item.h
@@ -5,8 +5,54 @@
 #ifndef MALL_ITEM_H
 #define MALL_ITEM_H
 
+#include <string>
 
 class Item {
+
+public:
+    // Constructor without parameters
+    Item();
+
+    // Init constructor
+    Item(std::string name, unsigned int price);
+
+    // Copy constructor
+    Item(const Item& item);
+
+    // Move constructor
+    Item(Item&& item);
+
+    /*
+     * FUNCTIONALITY
+     */
+
+    // TODO: implement basic Item functionality
+    /*
+     *  - change price (if there is a discountCard)
+     *  - return info
+     *  - from production date to this day
+     */
+
+    /*
+     * GETTERS AND SETTERS
+     */
+
+    // TODO: implement getters and setters
+
+
+
+
+    // Destructor
+    ~Item();
+
+private:
+    std::string name;
+    // TODO: add brand
+    // TODO: add pointer to Shop it belongs
+    unsigned int price;
+    // TODO: implement Date of production and expiration date
+    // TODO: implement price array for amount of items
+    std::string producer;
 
 };
 

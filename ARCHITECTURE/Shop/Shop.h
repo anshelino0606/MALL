00feb81eb599d25366f20cae2af94b77ac1e6733
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-22 22:16:28 +0300

    basic declarations added to Shop

--- ARCHITECTURE/Shop/Shop.h
+++ ARCHITECTURE/Shop/Shop.h
@@ -5,8 +5,66 @@
 #ifndef ENGINE_MALL_SHOP_H
 #define ENGINE_MALL_SHOP_H
 
+#include "../Item/Item.h"
 
 class Shop {
+
+public:
+
+    /*
+     * CONSTRUCTORS
+     */
+
+    // Without parameters
+    Shop();
+
+    // Init constructor
+    Shop(const std::string& name, unsigned int maxCapacity);
+
+    // Copy constructor
+    Shop(const Shop& other);
+
+    // Move constructor
+    Shop(Shop&& other) noexcept;
+
+    /*
+     * METHODS
+     */
+
+    /*
+     * Reads shop from the stream
+     * File is in the format of path to directory
+     */
+
+    void readFromFile(const std::string& fileName);
+
+    /*
+     * 1. Prints the shop to the console
+     * 2. Prints the shop to the stream
+     */
+    void print() const;
+    void print(std::ostream& os) const;
+
+
+
+
+    /*
+     * DESCTRUCTOR
+     */
+
+    ~Shop();
+
+private:
+
+    // Capacity of the shop
+    unsigned int maxCapacity;
+    unsigned int currentCapacity;
+
+    // Items array
+    Item* items;
+
+    // Shop name
+    std::string name;
 
 };
 

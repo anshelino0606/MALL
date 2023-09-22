91a0b13420ad1f3ec3f93c6970b5dcaff2834b1b
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-22 23:22:19 +0300

    ShoppingCart functionality declared

--- ARCHITECTURE/ShoppingCart/ShoppingCart.h
+++ ARCHITECTURE/ShoppingCart/ShoppingCart.h
@@ -8,7 +8,67 @@
 #include "../Item/Item.h"
 
 class ShoppingCart {
+public:
+    
+    /*
+     * CONSTRUCTORS
+     */
+    
+    // Without parameters
+    ShoppingCart();
+    
+    // Init constructor
+    ShoppingCart(unsigned int width, unsigned int height, Item* items);
+    
+    // Copy constructor
+    ShoppingCart(const ShoppingCart& shoppingCart);
+    
+    // Move constructor
+    ShoppingCart(ShoppingCart&& shoppingCart);
+    
+    /*
+     * FUNCTIONALITY
+     */
 
+    void addToCart(const Item& item);
+    void removeFromCart(const Item& item);
+    void printAllItems();
+    
+    /*
+     * GETTERS AND SETTERS
+     */
+    
+    float getHeightOfCart();
+    float getWidthOfCart();
+    Item* getItems();
+    
+    void setHeightOfCart(float height);
+    void setWidthOfCart(float width);
+
+    /*
+     * Calculates optimal space for items in cart
+     */
+    float calculateOptimalPlacement();
+    
+    /*
+     * GRAPHICS PART
+     */
+
+    void processInput(float dt);
+    void render();
+    void init();
+    void update(float dt);
+    void doCollisions();
+    void doOptimalPlacement();
+    
+private:
+    
+    float width;
+    float height;
+    
+    Item* items;
+    
+    bool isOpen;
 };
 
 

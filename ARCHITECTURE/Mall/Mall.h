1e67d1841e33dfa2f35aef6ec6d94826efca8a76
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-22 23:53:36 +0300

    Mall added declarations of functionality

--- ARCHITECTURE/Mall/Mall.h
+++ ARCHITECTURE/Mall/Mall.h
@@ -5,9 +5,39 @@
 #ifndef MALL_MALL_H
 #define MALL_MALL_H
 
+#include "../Shop/Shop.h"
 
 class Mall {
 
+public:
+
+    /*
+     * CONSTRUCTORS
+     */
+    Mall(unsigned int width, unsigned int height);
+
+    /*
+     * GRAPHICS
+     */
+
+    void init();
+    void processInput(float dt);
+    void update(float dt);
+    void render();
+    void doCollisions();
+
+
+    /*
+     * DESTRUCTOR
+     */
+    ~Mall();
+
+
+private:
+    unsigned int width;
+    unsigned int height;
+
+    Shop* shops;
 };
 
 

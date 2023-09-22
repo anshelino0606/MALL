c0fca1cad840d09104c5e59f19f3a522b2261381
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-22 23:16:13 +0300

    ShoppingCart init

---
+++ ARCHITECTURE/ShoppingCart/ShoppingCart.h
@@ -0,0 +1,15 @@
+//
+// Created by Anhelina Modenko on 22.09.2023.
+//
+
+#ifndef MALL_SHOPPINGCART_H
+#define MALL_SHOPPINGCART_H
+
+#include "../Item/Item.h"
+
+class ShoppingCart {
+
+};
+
+
+#endif //MALL_SHOPPINGCART_H

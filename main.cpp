319e0b9d9cd6b3a4fb369dc724ddae52e705ff0c
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-28 12:17:45 +0300

    main function implemented

--- main.cpp
+++ main.cpp
@@ -1,7 +1,176 @@
+#define GLFW_INCLUDE_GLCOREARB
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 832
+
+#include "ENGINE/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include "ARCHITECTURE/Mall/Mall.h"
+#include "ENGINE/IO/keyboard.h"
+
 #include <iostream>
 
-int main() {
-    std::cout << "Initial commit" << std::endl;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void process_input(GLFWwindow* window);
+void cursor_position_callback(GLFWwindow* window, double x, double y);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+// The Width of the screen
+const unsigned int SCREEN_WIDTH = WINDOW_WIDTH;
+// The height of the screen
+const unsigned int SCREEN_HEIGHT = WINDOW_HEIGHT;
+
+Mall Supermarket(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Supermarket", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // OpenGL configuration
+    // --------------------®®
+    int width = WINDOW_WIDTH;
+    int height = WINDOW_HEIGHT;
+    glViewport(0, 0, width, height);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // ImGUI config
+    // --------------
+    // Setup Dear ImGui context
+//    IMGUI_CHECKVERSION();
+//    ImGui::CreateContext();
+//    ImGuiIO& io = ImGui::GetIO();
+//    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+//    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+//
+//    // Setup Platform/Renderer backends
+//    ImGui_ImplGlfw_InitForOpenGL(window, true);
+//    ImGui_ImplOpenGL3_Init();
+
+    // initialize game
+    // ---------------
+    Supermarket.init();
+
+    // deltaTime variables
+    // -------------------
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+
+    int frameCounter = 0;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        process_input(window);
+        // update a screeen resolution
+        // ------------------
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        // calculate delta time
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        float fps = 1.0f / deltaTime;
+
+        glfwPollEvents();
+
+
+//        ImGui_ImplOpenGL3_NewFrame();
+//        ImGui_ImplGlfw_NewFrame();
+//        ImGui::NewFrame();
+//
+//        // manage user input
+//        // -----------------
+//        Supermarket.processInput(deltaTime);
+//
+//        ImGui::Begin("FPS COUNT");
+//        ImGui::Text("FPS: %.0f", fps);
+//        ImGui::End();
+
+        // update game state
+        // -----------------
+        Supermarket.update(deltaTime);
+
+        if (Supermarket.state == GameStates::GAME_ACTIVE) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        }
+
+        // render
+        // ------
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        Supermarket.render();
+        // Start the Dear ImGui frame
+
+
+//        ImGui::Render();
+//        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    // delete all resources as loaded using the resource manager
+    // ---------------------------------------------------------
+    ResourceManager::clear();
+
+//    ImGui_ImplOpenGL3_Shutdown();
+//    ImGui_ImplGlfw_Shutdown();
+//    ImGui::DestroyContext();
+
+    glfwTerminate();
 
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    Keyboard::keyCallback(window, key, scancode, action, mode);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if (Keyboard::key(GLFW_KEY_ESCAPE)) {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+}
+
+void cursor_position_callback(GLFWwindow* window, double x, double y) {
+    Mouse::cursorPosCallback(window, x, y);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+    Mouse::mouseButtonCallback(window, button, action, mods);
 }
